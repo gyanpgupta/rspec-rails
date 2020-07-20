88ac0a5c01061e10973b9cf192dc5a3d72fee249
Phil Pirozhkov <hello@fili.pp.ru>
2020-07-20 10:59:32 +0300

    Backport skipping of a spec due to Rails bug
    Update to the latest released 5.2.4.3 didn't help.
    
    Backported from https://github.com/rspec/rspec-rails/blob/c0a8c22186a1232e3b1612c44bc88052ec56c945/spec/rspec/rails/example/view_example_group_spec.rb#L239

--- spec/rspec/rails/example/view_example_group_spec.rb
+++ spec/rspec/rails/example/view_example_group_spec.rb
@@ -218,7 +218,18 @@ module RSpec::Rails
         expect(view_spec.view).to eq(view)
       end
 
-      it 'is accessible to hooks' do
+      if RUBY_VERSION <= "2.3.0" && ENV["RAILS_VERSION"] !~ /stable/ && ::Rails.version.to_f == 5.2
+        pending_only_on_ruby_22_rails_52 = """
+          Rails 5.2.4.2 has a syntax error in ActionDispatch::Request::Session.
+          (A &. usage which does not work in 2.2.10)
+          It has been fixed but not released, this spec will not pass until that
+          has been released.
+        """
+      else
+        pending_only_on_ruby_22_rails_52 = false
+      end
+
+      it 'is accessible to hooks', :pending => pending_only_on_ruby_22_rails_52 do
         group = RSpec::Core::ExampleGroup.describe 'a view', :type => :view do
           specify { true }
         end
